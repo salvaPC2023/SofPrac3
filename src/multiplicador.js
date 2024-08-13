85ad541153ede896b2d691814a35ab817c250518
Salva <salvador.patty@ucb.edu.bo>
2024-08-13 10:18:46 -0400

    Funcion Multiplicarimplementada

---
+++ src/multiplicador.js
@@ -0,0 +1,6 @@
+function multiplicar(a, b) {
+    return a * b;
+  }
+  
+  export default multiplicar;
+  
--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,31 @@
+import multiplicar from "./multiplicador";
 import sumar from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const sumarButton = document.querySelector("#sumar-button");
+const multiplicarButton = document.querySelector("#multiplicar-button");
 
-form.addEventListener("submit", (event) => {
+
+/*form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});*/
+
+sumarButton.addEventListener("click", () => {
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+multiplicarButton.addEventListener("click", () => {
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
